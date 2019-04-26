750c18fdac25288694f30eb424954fc83bab3fe1
AudreyJones <audrey.eliz.jones@gmail.com>
2019-04-26 14:27:03 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validate :category_must_be_a_fiction
+    validates :content, length: { minimum: 100 }
+
+
+
+    def category_must_be_a_fiction
+        if category == "Fiction" || category == "Non-Fiction"
+            true
+        else
+            errors.add(:category, "is not a type of fiction")
+        end
+    end
 end
